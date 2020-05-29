41f0de496b5fd46e0a388b2341c71e4eae27e44b
feng <feng@local.mbp>
2020-05-29 15:01:32 +0800

    delete

--- src/test/java/image/proxy/ProxyApplicationTests.java
+++ src/test/java/image/proxy/ProxyApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class ProxyApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	
+	public static void main(String args[]) {
+		Integer a = null;
+		if(a!=null &&a == 3) {
+			System.out.println("33333333");
+		}
 	}
 
 }
